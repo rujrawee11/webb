6ae77f09308a367433e61fa3c6b1cbd46af6e461
rujrawee11 <73680402+rujrawee11@users.noreply.github.com>
2020-10-30 12:08:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>myfirst heading</h1>
+        <p>myfirst heading</p>
+        <img src="cat.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+        <h2>hello</h2>
+
+    </body>
+</html>
