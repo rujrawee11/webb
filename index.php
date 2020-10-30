287d5242afa07a817e42d786119a3d0abfbb9c98
rujrawee11 <73680402+rujrawee11@users.noreply.github.com>
2020-10-30 12:34:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     </head>
     <body>
         <h1>myfirst heading</h1>
-        <p>myfirst heading</p>
+        <p>รุจรวี มีลา เนย</p>
         <img src="cat.jpg">
+        <img src="catt.jpg">
         <br>
-        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
-        <h2>hello</h2>
+        <a href="https://twitter.com/hashtag/%E0%B8%A1%E0%B8%B8%E0%B8%82%E0%B8%95%E0%B8%A5%E0%B8%81">This is a Link</a>
+        <h2>เหนื่อยก็พัก หนักก็วาง ชาเขียวปั่นอยู่ข้างๆ หยิบกินบ้างก็ได้</h2>
 
     </body>
 </html>
